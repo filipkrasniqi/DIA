593f89ed85b141e384cb68ec211df9fabe8a2843
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-10-24 09:56:17 +0200

    Function to save also image for GP

--- Code/Part_3/Comb_GPTS.py
+++ Code/Part_3/Comb_GPTS.py
@@ -32,7 +32,7 @@ def built_matrix_sub_budget_clicks_without_errors(n_arms, arms, n_sub_campaign, 
 '''
 Prepare environment for script
 '''
-sigma_env_n = [0.1]
+sigma_env_n = [0.01, 0.1, 1, 2]
 bid = 10
 
 prob_users_n = [
@@ -42,6 +42,13 @@ prob_users_n = [
         [0.50, 0.10, 0.40],
         [0.05, 0.05, 0.90],
         [0.10, 0.30, 0.60]
+    ],
+    [
+        [0.60, 0.20, 0.20],
+        [0.30, 0.40, 0.30],
+        [0.30, 0.60, 0.10],
+        [0.95, 0.05, 0.0],
+        [0.2, 0.70, 0.10]
     ]
 ]
 
@@ -49,19 +56,50 @@ curr_dir = os.getcwd()
 outputs_dir = curr_dir+"/outputs/"
 if not os.path.exists(outputs_dir):
     os.mkdir(outputs_dir)
-env_dir = outputs_dir+"prova_finale/"
+env_dir = outputs_dir+"test1/"
 if not os.path.exists(env_dir):
     os.mkdir(env_dir)
 
 n_sub_campaign = 5
 n_users_x_sub_campaign = 3
 n_arms_sub = 21
-total_budget = 200
+total_budget = 100
 
 min_daily_budget = 0.0
 max_daily_budget = total_budget
 
-T = 100
+T = 20
+
+def plot_regression(cur_fold, arms, env, idx_subcampaign, save_figure = False):
+    # Plot every subcampaign.
+    real_function_y = []
+    for a in arms:
+        real_function_y.append(env.get_clicks_real(a, idx_subcampaign))
+
+    p = gpts_learners[i].arms[pulled_arm]
+    reward = env.get_click_noise(p, i)
+    gpts_learners[i].update(pulled_arm, reward)
+
+    x_pred = np.atleast_2d(arms).T
+
+    y = np.array(gpts_learners[i].means)
+    sigmaGP = np.array(gpts_learners[i].sigmas)
+
+    plt.figure(t)
+    plt.plot(x_pred, real_function_y, 'r:', label=r'$Real Function$')
+    plt.plot(x_pred, y, 'b-', label=u'Predicted Rewards')
+    plt.fill(np.concatenate([x_pred, x_pred[::-1]]),
+             np.concatenate([y - 1.96 * sigmaGP, (y + 1.96 * sigmaGP)[::-1]]),
+             alpha=0.5, fc='b', ec='None', label='95% conf interval')
+
+    plt.xlabel('$x$')
+    plt.ylabel('$Real Function$')
+    plt.legend(loc='lower right')
+    plt.title("Subcampaign {}".format(i))
+    if save_figure:
+        plt.savefig('{}/prediction_subcampaign_{}.png'.format(cur_fold, idx_subcampaign))
+
+    plt.show()
 
 for k, s_p in enumerate(itertools.product(sigma_env_n, prob_users_n)):
     sigma_env = s_p[0]
@@ -76,6 +114,7 @@ for k, s_p in enumerate(itertools.product(sigma_env_n, prob_users_n)):
 
     env = Environment(n_arms_sub, n_users_x_sub_campaign, n_sub_campaign, total_budget, bid, prob_users, sigma_env)
     arms = env.get_arms()
+    env.plot()
 
     # Val ottimo per calcolare Regret
     matrix = built_matrix_sub_budget_clicks_without_errors(n_arms_sub, arms, n_sub_campaign, env)
@@ -120,38 +159,9 @@ for k, s_p in enumerate(itertools.product(sigma_env_n, prob_users_n)):
             pulled_arm = int(np.where(gpts_learners[i].arms == pulled_arms[i])[0])
             gpts_learners[i].update(pulled_arm, rewards[i])
 
-            if t % 10 == 0:
+            if t % 5 == 0:
                 # Plot every subcampaign.
-                real_function_y = []
-                for a in arms:
-                    real_function_y.append(env.get_clicks_real(a, i))
-
-                p = gpts_learners[i].arms[pulled_arm]
-                reward = env.get_click_noise(p, i)
-                gpts_learners[i].update(pulled_arm, reward)
-
-                arm_obs = np.append(arm_obs, arms[pulled_arm])
-
-                X = np.atleast_2d(arm_obs).T
-                Y = gpts_learners[i].collected_rewards.ravel()
-                x_pred = np.atleast_2d(arms).T
-
-                y = np.array(gpts_learners[i].means)
-                sigmaGP = np.array(gpts_learners[i].sigmas)
-
-                plt.figure(t)
-                plt.plot(x_pred, real_function_y, 'r:', label=r'$Real Function$')
-                #plt.plot(X.ravel(), Y, 'ro', label=u'Observed Arms')
-                plt.plot(x_pred, y, 'b-', label=u'Predicted Rewards')
-                plt.fill(np.concatenate([x_pred, x_pred[::-1]]),
-                         np.concatenate([y - 1.96 * sigmaGP, (y + 1.96 * sigmaGP)[::-1]]),
-                         alpha=0.5, fc='b', ec='None', label='95% conf interval')
-
-                plt.xlabel('$x$')
-                plt.ylabel('$Real Function$')
-                plt.legend(loc='lower right')
-                plt.title("Subcampaign {}".format(i))
-                plt.show()
+                plot_regression(cur_fold, arms, env, i)
 
         rewards_per_round.append(np.sum(rewards))
 
@@ -181,6 +191,16 @@ for k, s_p in enumerate(itertools.product(sigma_env_n, prob_users_n)):
     plt.plot(regression_error, 'b')
     plt.savefig(cur_fold+'/avrregerr.png')
     plt.show()
+
+    # save also for each subcampaign what came up from the GP
+    for i in range(n_sub_campaign):
+        matrix = built_matrix_sub_budget_clicks(n_arms_sub, arms, n_sub_campaign, gpts_learners)
+        combinatorial_alg = DPAlgorithm(arms, n_sub_campaign, matrix, min_daily_budget, total_budget)
+        combinatorial = combinatorial_alg.get_budgets()
+        pulled_arms = combinatorial[1]
+        pulled_arm = int(np.where(gpts_learners[i].arms == pulled_arms[i])[0])
+        arm_obs = np.append(arm_obs, arms[pulled_arm])
+        plot_regression(cur_fold, arms, env, i, save_figure=True)
 
     log = str(sigma_env)+"\n"+str(prob_users)
     f = open(cur_fold+"/log.txt","w")
