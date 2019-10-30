23b02cf010d03cfcf30b651d5f387e88f3d876c3
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-10-30 18:03:10 +0100

    Updated for correct visualization and tracking of regret

--- Code/Part_1/ProjectEnvironment.py
+++ Code/Part_1/ProjectEnvironment.py
@@ -1,38 +1,49 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from Code.Part_1.Environment import Environment
 
-season_length = 90
+season_length = 360
 number_of_seasons = 4
 
 class ProjectEnvironment(Environment):
-    def __init__(self, n_arms, probabilities, sigma, matrix_parameters):
-        self.n_arms = n_arms
+    def __init__(self, arms, probabilities, sigma, matrix_parameters):
+        self.arms = arms
+        self.n_arms = len(self.arms)
         self.probabilities = probabilities
         self.sigma = sigma
-        self.users = [User(i,parameters, self.sigma) for i,parameters in enumerate(matrix_parameters)]
+        self.users = [User(i, arms, parameters, self.sigma) for i,parameters in enumerate(matrix_parameters)]
+        self.regret = []
 
     def round(self, pulled_arm, t):
-        choices = [i for i in range(self.n_users)]
-        user = np.random.choice(choices, 1, self.probabilities)
-        sample = self.users[user].update_samples(pulled_arm, t)  # domanda
-        return sample * pulled_arm  # reward
+        choices = [i for i in range(len(self.users))]
+        user = np.random.choice(choices, 1, self.probabilities)[0]
+        sample = self.users[user].update_samples(pulled_arm, t)  # demand
+        reward = sample * pulled_arm
+        real_sample = self.users[user].demand(pulled_arm, t)
+        real_reward = real_sample * pulled_arm
+        optimum, optimum_arm = self.users[user].optimum(t)
+        self.regret.append(optimum - real_reward)
+        return reward, user  # reward
 
     def season(self, t):
         return int((t % 365) / season_length)
 
-    def plot(self, n_arms):
+    def plot(self):
         for u in self.users:
-            u.plot(n_arms)
+            u.plot()
 
 
 class User():
-    def __init__(self,idx,parameters, sigma):
+    def __init__(self, idx, arms, parameters, sigma):
         self.phase_function = [(s,D,f_smooth) for s,D,f_smooth in parameters]
         self.samples = []
         self.sigma = sigma
         self.idx = idx
+        self.arms = arms
+        self.n_arms = len(self.arms)
 
     def update_samples(self, price, t):
         sample = self.noised_demand(price, t)
@@ -40,18 +51,24 @@ class User():
         return sample
 
     def noised_demand(self, price, t):
-        return self.demand(price, t) + np.random.normal(0, self.sigma)
+        return max(0, self.demand(price, t) + np.random.normal(0, self.sigma))
 
     def demand(self, price, t, season = None):
         if season is None:
             season = self.season(t)
+        if season >= number_of_seasons:
+            print("mierda")
         return np.exp(price * (-1) * self.phase_function[season][0]) * self.phase_function[season][1] * self.phase_function[season][2](t)
 
-    def season(self, t):
-        return int((t % 365) / season_length)
+    def optimum(self, t, season = None):
+        all_demands = [self.demand(arm, t, season) * arm for arm in self.arms]
+        return np.max(all_demands), np.argmax(all_demands)
 
-    def plot(self, n_arms):
-        bins = np.linspace(0,n_arms)
+    def season(self, t):
+        return int((t / season_length)) % number_of_seasons
+
+    def plot(self):
+        bins = np.linspace(0,max(self.arms))
         seasons = list(range(number_of_seasons))
         demands = []
         for s in seasons:
