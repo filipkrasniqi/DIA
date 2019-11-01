c5f3f0bc6dff2c8ba748bc538c2b24d986977471
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-01 19:52:43 +0100

    Added computation of best alternative among the contexts

--- Code/Part_1/run.py
+++ Code/Part_1/run.py
@@ -10,7 +10,7 @@ from Code.Part_1.UCB_Learner import UCB_Learner
 
 probabilities = [0.4, 0.3, 0.3]
 sigma_env_n = [2]
-T = 100
+T = 600
 
 
 def linear(t):
@@ -37,16 +37,38 @@ test_T = int(portion_samples_ab_testing*T)
 norm_dist = stats.norm(0, 1)
 z_a, z_b = norm_dist.pdf(1-alpha), norm_dist.pdf(beta)
 do_sequential_AB = False
-do_UCB = False
+do_UCB = True
 do_TS = False
 
 do_UCB_wdw = False
-do_TS_wdw = True
+do_TS_wdw = False
 
 def train(learner):
     for sigma in sigma_env_n:
         env = Environment(arms, probabilities, sigma, matrix_parameters)
         for t in range(T):
+            learner.pull_arm(env, t)
+        learner.plot(env)
+
+def train_context(learner):
+    for sigma in sigma_env_n:
+        env = Environment(arms, probabilities, sigma, matrix_parameters)
+        for t in range(T):
+            if t % 33 == 32:
+                context_alternatives = [[[0,1,2]], [[0,1],[2]], [[0,2],[1]], [[1,2],[0]], [[0],[1],[2]]]
+                results_alternatives = []
+                for alternative in context_alternatives:
+                    sum = 0
+                    for context in alternative:
+                        is_all = len(context) == len(probabilities)
+                        mu,  prob = learner.avg_bounds(context, alpha), learner.prob_lower_bound(context, alpha)
+                        if is_all:
+                            sum += min(mu)
+                        else:
+                            sum += prob * min(mu)
+                        results_alternatives.append(sum)
+                    # print("Alternative {}: {}".format(alternative, sum))
+                print("Best alternative: {}".format(np.argmax(results_alternatives)))
             learner.pull_arm(env, t)
         learner.plot(env)
 
@@ -56,7 +78,8 @@ if do_sequential_AB:
 
 # 2) UCB
 if do_UCB:
-    train(learner=UCB_Learner(arms))
+    train_context(learner=UCB_Learner(arms))
+    # train(learner=UCB_Learner(arms))
 
 # 3) TS
 if do_TS:
