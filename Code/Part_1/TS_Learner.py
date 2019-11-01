7ba23f4da8b5b1c6c3832e18a840b4acfe4db18e
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-01 16:38:13 +0100

    Updated TS learner for bernoulli

--- Code/Part_1/TS_Learner.py
+++ Code/Part_1/TS_Learner.py
@@ -1,18 +1,21 @@
-from Learner import *
 import numpy as np
 
+from Code.Part_1.Learner import Learner
+
 class TS_Learner(Learner):
-    def __init__(self,n_arms):
-        super().__init__(n_arms)
+    def __init__(self,arms):
+        super().__init__(arms)
         # 2 parameter for each arm
-        self.beta_parameters = np.ones((n_arms,2))
+        self.beta_parameters = np.ones((self.n_arms,2))
 
-    def pull_arm(self):
-        idx = np.argmax(np.random.beta(self.beta_parameters[:,0],self.beta_parameters[:,1]))
-        return idx
+    def pull_arm(self, env, t):
+        idx_arm = np.argmax(np.random.beta(self.beta_parameters[:,0],self.beta_parameters[:,1]))
+        Learner.pull_arm(self, env, t, idx_arm)
+        return idx_arm
 
-    def update(self,pulled_arm,reward):
+    def update(self,pulled_arm,reward, user):
         self.t += 1
-        self.update_observations(pulled_arm,reward)
+        # TODO not ok to compute with reward. We must just add whether choice was correct or not to correct (alfa, beta)
+        self.update_observations(pulled_arm,reward, user)
         self.beta_parameters[pulled_arm,0] = self.beta_parameters[pulled_arm,0] + reward
         self.beta_parameters[pulled_arm,1] = self.beta_parameters[pulled_arm,1] + 1.0 - reward
