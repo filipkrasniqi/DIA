9dc0879db2349836162a0101d68bee2d057d1dcd
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-01 16:38:43 +0100

    Updated UCB to work with current implementation of Learner

--- Code/Part_1/UCB_Learner.py
+++ Code/Part_1/UCB_Learner.py
@@ -1,23 +1,34 @@
-from Learner.Learner import *
 import numpy as np
 import math
 
+from Code.Part_1.Learner import Learner
+
 class UCB_Learner(Learner):
-    def __init__(self,n_arms):
-        super().__init__(n_arms)
+    def __init__(self,arms, window = None):
+        super().__init__(arms)
+        self.window = window
         # Upper Bound = AVG reward + Delta
-        self.average_rewards = np.zeros(n_arms)
-        self.delta = np.zeros(n_arms)
+        self.average_rewards = np.zeros(self.n_arms)
+        self.delta = np.zeros(self.n_arms)
 
-    def pull_arm(self):
+    def pull_arm(self, env, t):
         if (self.t < self.n_arms):
-            return self.t
+            idx_arm = self.t
+        else:
+            idx_arm = np.argmax(self.average_rewards + self.delta)
+        Learner.pull_arm(self, env, t, idx_arm)
+        return idx_arm
 
-        arm =  np.argmax(self.average_rewards + self.delta)
-        return arm
-
-    def update(self, pulled_arm, reward):
+    def update(self, pulled_arm, reward, user):
         self.t += 1
-        self.update_observations(pulled_arm, reward)
-        self.average_rewards[pulled_arm] = np.sum (self.rewards_per_arm[pulled_arm]) / len(self.rewards_per_arm[pulled_arm])
-        self.delta[pulled_arm] = math.sqrt(2 * math.log(self.t) / len(self.rewards_per_arm[pulled_arm] ))
+        self.update_observations(pulled_arm, reward, user)
+
+        tot_n = len(self.rewards_per_arm[pulled_arm])
+        if self.window is None or self.window > tot_n:
+            n = tot_n
+        else:
+            n = self.window
+        idxs = list(range(tot_n - n, tot_n))
+
+        self.average_rewards[pulled_arm] = np.sum([r for i, r in enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / n
+        self.delta[pulled_arm] = math.sqrt(2 * math.log(self.t) / n)
