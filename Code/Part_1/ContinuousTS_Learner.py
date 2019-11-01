c3ade29049ce497a36d5c894c5784d14c41afba6
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-01 19:51:29 +0100

    Removed guarantee of exploration as in UCB. Fixed unexplained bug.

--- Code/Part_1/ContinuousTS_Learner.py
+++ Code/Part_1/ContinuousTS_Learner.py
@@ -4,16 +4,12 @@ from Code.Part_1.Learner import Learner
 
 class TS_Learner(Learner):
     def __init__(self, arms, window = None):
-        Learner.__init__(self, arms)
-        self.window = window
+        Learner.__init__(self, arms, window=window)
         self.average_rewards = [0 for _ in range(self.n_arms)]
         self.rewards_variance = [float("+inf") for _ in range(self.n_arms)]
 
     def pull_arm(self, env, t):
-        if (self.t < self.n_arms):
-            idx_arm = self.t
-        else:
-            idx_arm = np.argmax(np.random.normal(self.average_rewards,np.power(self.rewards_variance, 0.5)))
+        idx_arm = np.argmax(np.random.normal(self.average_rewards,np.power(self.rewards_variance, 0.5)))
         Learner.pull_arm(self, env, t, idx_arm)
         return idx_arm
 
@@ -22,9 +18,9 @@ class TS_Learner(Learner):
         self.update_observations(pulled_arm, reward, user)
         tot_n = len(self.rewards_per_arm[pulled_arm])
         if self.window is None or self.window > tot_n:
-            n = tot_n
+            n = int(tot_n)
         else:
-            n = self.window
+            n = int(self.window)
         idxs = list(range(tot_n - n, tot_n))
         self.average_rewards[pulled_arm] = np.sum([r for i, r in enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / n
         self.rewards_variance[pulled_arm] = np.sum(
