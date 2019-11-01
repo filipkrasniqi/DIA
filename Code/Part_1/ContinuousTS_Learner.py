edee6eef9d1d6afc14a6c329c6f5cae6154191fd
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-01 16:36:33 +0100

    Implementation of continuous TS learner

---
+++ Code/Part_1/ContinuousTS_Learner.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from Code.Part_1.Learner import Learner
+
+class TS_Learner(Learner):
+    def __init__(self, arms, window = None):
+        Learner.__init__(self, arms)
+        self.window = window
+        self.average_rewards = [0 for _ in range(self.n_arms)]
+        self.rewards_variance = [float("+inf") for _ in range(self.n_arms)]
+
+    def pull_arm(self, env, t):
+        if (self.t < self.n_arms):
+            idx_arm = self.t
+        else:
+            idx_arm = np.argmax(np.random.normal(self.average_rewards,np.power(self.rewards_variance, 0.5)))
+        Learner.pull_arm(self, env, t, idx_arm)
+        return idx_arm
+
+    def update(self, pulled_arm, reward, user):
+        self.t += 1
+        self.update_observations(pulled_arm, reward, user)
+        tot_n = len(self.rewards_per_arm[pulled_arm])
+        if self.window is None or self.window > tot_n:
+            n = tot_n
+        else:
+            n = self.window
+        idxs = list(range(tot_n - n, tot_n))
+        self.average_rewards[pulled_arm] = np.sum([r for i, r in enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / n
+        self.rewards_variance[pulled_arm] = np.sum(
+            [(reward - self.average_rewards[pulled_arm]) ** 2 for i, reward in enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / n - 1
+    def get_arm_mean(self, arm):
+        return self.average_rewards[arm]
