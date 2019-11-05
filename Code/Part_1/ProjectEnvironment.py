5630c3cc74c4449f8b0e3758fa9f4e1825a0cdb8
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-05 18:57:38 +0100

    Log better. Fixed problem of more samples

--- Code/Part_1/ProjectEnvironment.py
+++ Code/Part_1/ProjectEnvironment.py
@@ -2,17 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from Code.Part_1.Environment import Environment
+import itertools
 
 season_length = 720
 number_of_seasons = 4
 
 
 class ProjectEnvironment(Environment):
-    def __init__(self, arms, probabilities, sigma, context_matrix_parameters, context_alternatives):
+    def __init__(self, arms, probabilities, sigma, context_matrix_parameters, context_alternatives, batch_size = 16):
         self.arms = arms
         self.n_arms = len(self.arms)
         self.probabilities = probabilities
         self.sigma = sigma
+        self.batch_size = batch_size
         """
         List of contexts.
         A single context is a list of subcontexts.
@@ -60,32 +62,49 @@ class ProjectEnvironment(Environment):
         self.all_rewards.append(samples)
         return samples, user
 
-    def round_for_arm(self, pulled_arm, t, user=None):
-        choices = [i for i in range(len(self.contexts[self.selected_context]))]
-        users = self.contexts[self.selected_context]
-        subcontexts = self.contexts_alternatives[self.selected_context]
-        if user is None:
-            user = np.random.choice(choices, 1, self.probabilities)[0]
-        subcontext = self.get_subcontext_from_user(subcontexts, user)
-        idx_arm = [idx_arm for idx_arm, arm in enumerate(self.arms) if arm == pulled_arm][0]
-        reward = self.all_rewards[-1][idx_arm]
-        real_sample = users[subcontext].demand(pulled_arm, t)
-        real_reward = real_sample * pulled_arm
-        contexts_optimals = []
-        for idx_c, context in enumerate(self.contexts):
-            context_optimal = 0
-            for user, p in zip(context, self.probabilities_context(idx_c)):
-                optimum, optimum_arm = user.optimum(t)
-                context_optimal += p * optimum
-            contexts_optimals.append(context_optimal)
-        best_context = np.max(contexts_optimals)
-        self.regret.append(best_context - real_reward)
-        self.real_rewards.append(real_reward)
-        self.drawn_users.append(user)
+    def round_context(self, t):
+        samples, subcontexts_to_return, users_to_return = [[] for _ in self.contexts], [[] for _ in self.contexts], [[] for _ in self.contexts]
+        list_attempts = list(range(self.batch_size))
+        for idx_attempt, (idx_c, context) in itertools.product(list_attempts, enumerate(self.contexts)):
+            subcontext, user = self.sample_subcontext(idx_c)
+            subcontexts_to_return[idx_c].append(subcontext)
+            users_to_return[idx_c].append(user)
+            users = context
+            for arm in self.arms:
+                samples[idx_c].append(users[subcontext].update_samples(arm, t) * arm)  # demand
+
+        for samples_context in samples:
+            for sample in samples_context:
+                self.all_rewards.append(sample)
+        return samples, subcontexts_to_return, users_to_return
+
+    def round_for_arm(self, idx_pulled_arms, t, users):
+        for idx, (idx_arm, user) in enumerate(zip(idx_pulled_arms, users)):
+            users = self.contexts[self.selected_context]
+            subcontexts = self.contexts_alternatives[self.selected_context]
+            subcontext = self.get_subcontext_from_user(subcontexts, user)
+            pulled_arm = self.arms[idx_arm]
+            idx_reward = self.batch_size - idx
+            reward = self.all_rewards[idx_reward]
+            real_sample = users[subcontext].demand(pulled_arm, t)
+            real_reward = real_sample * pulled_arm
+            contexts_optimals = []
+            for idx_c, context in enumerate(self.contexts):
+                context_optimal = 0
+                for user, p in zip(context, self.probabilities_context(idx_c)):
+                    optimum, optimum_arm = user.optimum(t)
+                    context_optimal += p * optimum
+                contexts_optimals.append(context_optimal)
+            best_context = np.max(contexts_optimals)
+            self.regret.append(best_context - real_reward)
+            self.real_rewards.append(real_reward)
+            self.drawn_users.append(user)
         return reward, user
 
-    def sample_subcontext(self):
-        subcontexts = self.contexts_alternatives[self.selected_context]
+    def sample_subcontext(self, context = None):
+        if context is None:
+            context = self.selected_context
+        subcontexts = self.contexts_alternatives[context]
         user = np.random.choice([i for i in range(len(self.probabilities))], 1, self.probabilities)[0]
         subcontext = self.get_subcontext_from_user(subcontexts, user)
         return subcontext, user
@@ -100,10 +119,19 @@ class ProjectEnvironment(Environment):
     def season(self, t):
         return int((t % 365) / season_length)
 
-    def plot(self):
-        users = self.contexts[self.selected_context]
-        for u in users:
-            u.plot()
+    def plot(self, contexts = None):
+        if contexts is None:
+            contexts = self.contexts
+        elif isinstance(contexts, list):
+            # expect list of scalars
+            contexts = [self.contexts[idx_c] for idx_c in contexts]
+        else:
+            # expects single scalar
+            contexts = [self.contexts[contexts]]
+
+        for idx_c, users in enumerate(contexts):
+            for u in users:
+                u.plot(idx_c)
 
 
 """
@@ -137,8 +165,9 @@ class User():
             season = self.season(t)
         if season >= number_of_seasons:
             print("mierda")
-        return np.exp(price * (-1) * self.phase_function[season][0]) * self.phase_function[season][1] * \
-               self.phase_function[season][2](t)
+        return self.phase_function[season][2](t)
+        # return np.exp(price * (-1) * self.phase_function[season][0]) * self.phase_function[season][1] + \
+        #        self.phase_function[season][2](t)
 
     def optimum(self, t, season=None):
         all_demands = [self.demand(arm, t, season) * arm for arm in self.arms]
@@ -147,7 +176,7 @@ class User():
     def season(self, t):
         return int((t / season_length)) % number_of_seasons
 
-    def plot(self):
+    def plot(self, idx_context):
         bins = np.linspace(0, max(self.arms))
         seasons = list(range(number_of_seasons))
         demands = []
@@ -159,9 +188,9 @@ class User():
 
         for s, demand in enumerate(demands):
             plt.plot(bins, demand)
-            plt.title("Demand Curve User {},Season {}".format(self.idx, s))
+            plt.title("Context {} \nDemand Curve User {},Season {}".format(idx_context, self.idx, s))
             plt.xlabel("Prices")
             plt.xlim((0, np.max(bins)))
-            plt.ylim(0, np.max(demand))
+            plt.ylim(0, np.max(demand)+(1/10)*np.max(demand))
             plt.ylabel("Demand")
             plt.show()
