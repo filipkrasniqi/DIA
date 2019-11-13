434152478ebea4990dcb87abff1706e948cc9cc8
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2019-11-13 09:01:08 +0100

    Added comments. Fixed bug in computation of variance

--- Code/Part_1/ContinuousTS_Learner.py
+++ Code/Part_1/ContinuousTS_Learner.py
@@ -2,20 +2,31 @@ import numpy as np
 
 from Code.Part_1.Learner import Learner
 
-
+'''
+Thompson-Sampling learner. Each arm is associated
+to a Gaussian distribution, on which we estimate mean and std.
+'''
 class TS_Learner(Learner):
-    def __init__(self, arms, window=None, idx_c=-1, idx_s=-1):
-        super().__init__(arms, window=window, idx_c=idx_c, idx_s=idx_s)
+    def __init__(self, arms, idx_c, idx_s, sigma, window=None):
+        name_learner = "Thompson-Sampling"
+        if window is not None:
+            name_learner += " with window {}".format(window)
+        super().__init__(arms, window=window, idx_c=idx_c, idx_s=idx_s, name_learner=name_learner, sigma = sigma)
         self.average_rewards = [0 for _ in range(self.n_arms)]
         self.rewards_variance = [float("+inf") for _ in range(self.n_arms)]
 
-    def pull_arm(self, rewards_per_arm, user, t):
+    '''
+    Pull arm s.t. sampling from the Gaussian is higher.
+    '''
+    def pull_arm(self, rewards_per_arm, demands_per_arm, user, t):
         idx_arm = np.argmax(np.random.normal(self.average_rewards, np.power(self.rewards_variance, 0.5)))
-        return Learner.pull_arm(self, rewards_per_arm, user, t, idx_arm)
-
-    def update(self, pulled_arm, reward, user):
+        return Learner.pull_arm(self, rewards_per_arm, demands_per_arm, user, t, idx_arm)
+    '''
+    Updates Learner observations + own rewards mean and std
+    '''
+    def update(self, pulled_arm, reward, demand, user):
         self.t += 1
-        self.update_observations(pulled_arm, reward, user)
+        self.update_observations(pulled_arm, reward, demand, user)
         tot_n = len(self.rewards_per_arm[pulled_arm])
         if self.window is None or self.window > tot_n:
             n = int(tot_n)
@@ -26,7 +37,9 @@ class TS_Learner(Learner):
             [r for i, r in enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / n
         self.rewards_variance[pulled_arm] = np.sum(
             [(reward - self.average_rewards[pulled_arm]) ** 2 for i, reward in
-             enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / n - 1
-
+             enumerate(self.rewards_per_arm[pulled_arm]) if i in idxs]) / (n - 1)
+    '''
+    Returns mean given arm
+    '''
     def get_arm_mean(self, arm):
         return self.average_rewards[arm]
